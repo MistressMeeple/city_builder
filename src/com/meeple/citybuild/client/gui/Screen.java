000b74a757e124342b75c11ec8fe0a6857216d65
Mistress_Meeple <jskynol001@gmail.com>
2019-11-14 21:56:42 +0000

    quick fix to the screen class

--- src/com/meeple/citybuild/client/gui/Screen.java
+++ src/com/meeple/citybuild/client/gui/Screen.java
@@ -1,8 +1,6 @@
 package com.meeple.citybuild.client.gui;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import com.meeple.components.IDComponent;
@@ -10,14 +8,33 @@ import com.meeple.components.NamedComponent;
 import com.meeple.shared.frame.nuklear.NuklearUIComponent;
 
 public class Screen implements NamedComponent, IDComponent<Integer> {
-	private Map<String, Object> components = new HashMap<>();
+	
 	Set<NuklearUIComponent> uiComponents = new HashSet<>();
 
 	@Override
-	public Map<String, Object> getComponents() {
+	public Integer getID() {
 		// TODO Auto-generated method stub
-		return components;
+		return null;
 	}
+
+	@Override
+	public void setID(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 	//	public abstract void render();
 
