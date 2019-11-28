4131ea34570fe69c8e9f32a44a2ff3f41d05cd71
Mistress_Meeple <jskynol001@gmail.com>
2019-11-28 16:26:58 +0000

    patch to the io util reader class. copied from LWJGL demos

--- src/com/meeple/shared/frame/nuklear/IOUtil.java
+++ src/com/meeple/shared/frame/nuklear/IOUtil.java
@@ -1,22 +1,36 @@
 package com.meeple.shared.frame.nuklear;
 
-import static org.lwjgl.BufferUtils.*;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.SeekableByteChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.lwjgl.BufferUtils;
 
 public final class IOUtil {
 
 	private IOUtil() {
+	}
+
+	public static void writeToBuffer(InputStream stream, ByteBuffer buffer) throws IOException {
+		try (ReadableByteChannel rbc = Channels.newChannel(stream)) {
+			while (true) {
+				int bytes = rbc.read(buffer);
+				if (bytes == -1) {
+					break;
+				}
+				if (buffer.remaining() == 0) {
+					buffer = resizeBuffer(buffer, buffer.capacity() * 3 / 2); // 50%
+					System.out.println(buffer.capacity() + " " + buffer.remaining() + " " + buffer.hasRemaining());
+				}
+			}
+		}
 	}
 
 	private static ByteBuffer resizeBuffer(ByteBuffer buffer, int newCapacity) {
@@ -38,39 +52,37 @@ public final class IOUtil {
 	 */
 	public static ByteBuffer ioResourceToByteBuffer(String resource, int bufferSize) throws IOException {
 		ByteBuffer buffer;
-
-		Path path = Paths.get(resource);
-		path = path.toAbsolutePath();
-		if (Files.isReadable(path)) {
-			try (SeekableByteChannel fc = Files.newByteChannel(path)) {
-				buffer = BufferUtils.createByteBuffer((int) fc.size() + 1);
-				while (fc.read(buffer) != -1) {
-					;
-				}
-			}
+		URL url = Thread.currentThread().getContextClassLoader().getResource(resource);
+		if (url == null)
+			throw new IOException("Classpath resource not found: " + resource);
+		File file = new File(url.getFile());
+		if (file.isFile()) {
+			FileInputStream fis = new FileInputStream(file);
+			FileChannel fc = fis.getChannel();
+			buffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+			fc.close();
+			fis.close();
 		} else {
-			try (InputStream source = IOUtil.class.getClassLoader().getResourceAsStream(resource)) {
-				buffer = createByteBuffer(bufferSize);
-				writeToBuffer(source, buffer);
+			buffer = BufferUtils.createByteBuffer(bufferSize);
+			InputStream source = url.openStream();
+			if (source == null)
+				throw new FileNotFoundException(resource);
+			try {
+				byte[] buf = new byte[8192];
+				while (true) {
+					int bytes = source.read(buf, 0, buf.length);
+					if (bytes == -1)
+						break;
+					if (buffer.remaining() < bytes)
+						buffer = resizeBuffer(buffer, Math.max(buffer.capacity() * 2, buffer.capacity() - buffer.remaining() + bytes));
+					buffer.put(buf, 0, bytes);
+				}
+				buffer.flip();
+			} finally {
+				source.close();
 			}
 		}
-
-		buffer.flip();
 		return buffer;
-	}
-
-	public static void writeToBuffer(InputStream stream, ByteBuffer buffer) throws IOException {
-		try (ReadableByteChannel rbc = Channels.newChannel(stream)) {
-			while (true) {
-				int bytes = rbc.read(buffer);
-				if (bytes == -1) {
-					break;
-				}
-				if (buffer.remaining() == 0) {
-					buffer = resizeBuffer(buffer, buffer.capacity() * 3 / 2); // 50%
-				}
-			}
-		}
 	}
 
 }
