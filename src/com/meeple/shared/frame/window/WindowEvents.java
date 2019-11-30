be6441822b38a0fc988094467ef163592d87e672
Mistress_Meeple <jskynol001@gmail.com>
2019-11-30 18:10:41 +0000

    removing unused events

--- src/com/meeple/shared/frame/window/WindowEvents.java
+++ src/com/meeple/shared/frame/window/WindowEvents.java
@@ -8,23 +8,19 @@ import com.meeple.shared.Tickable;
 
 public class WindowEvents {
 
+	//rendering events
 	public Set<Runnable> frameStart = new CollectionSuppliers.SetSupplier<Runnable>().get();
 	public Set<Runnable> preClear = new CollectionSuppliers.SetSupplier<Runnable>().get();
 	public List<Tickable> render = new CollectionSuppliers.ListSupplier<Tickable>().get();
 	public List<Tickable> postRender = new CollectionSuppliers.ListSupplier<Tickable>().get();
 	public Set<Runnable> frameEnd = new CollectionSuppliers.SetSupplier<Runnable>().get();
 
+	//creation events
 	public Set<Runnable> preCreation = new CollectionSuppliers.SetSupplier<Runnable>().get();
 	public Set<Runnable> postCreation = new CollectionSuppliers.SetSupplier<Runnable>().get();
 
+	//destruction events
 	public Set<Runnable> preCleanup = new CollectionSuppliers.SetSupplier<Runnable>().get();
 	public Set<Runnable> postCleanup = new CollectionSuppliers.SetSupplier<Runnable>().get();
-
-	public Set<Runnable> preThreadStart = new CollectionSuppliers.SetSupplier<Runnable>().get();
-	/**
-	 * called AFTER capabilities have been set
-	 */
-	public Set<Runnable> postThreadStart = new CollectionSuppliers.SetSupplier<Runnable>().get();
-	public Set<Runnable> threadEnd = new CollectionSuppliers.SetSupplier<Runnable>().get();
 
 }
