7008100332ad228b0c1c6e8603fec29ef72e3af3
Mistress_Meeple <jskynol001@gmail.com>
2019-12-09 12:01:13 +0000

    removed error check. unused/didnt work

--- src/com/meeple/shared/frame/FrameUtils.java
+++ src/com/meeple/shared/frame/FrameUtils.java
@@ -128,38 +128,7 @@ public class FrameUtils {
 		values.add(value);
 	}
 
-	public static int glErrorCheck(String fileName, int line) {
-		int errorCode;
-		while ((errorCode = GL46.glGetError()) != GL46.GL_NO_ERROR) {
-			String error = "";
-			switch (errorCode) {
-				case GL46.GL_INVALID_ENUM:
-					error = "INVALID_ENUM";
-					break;
-				case GL46.GL_INVALID_VALUE:
-					error = "INVALID_VALUE";
-					break;
-				case GL46.GL_INVALID_OPERATION:
-					error = "INVALID_OPERATION";
-					break;
-				case GL46.GL_STACK_OVERFLOW:
-					error = "STACK_OVERFLOW";
-					break;
-				case GL46.GL_STACK_UNDERFLOW:
-					error = "STACK_UNDERFLOW";
-					break;
-				case GL46.GL_OUT_OF_MEMORY:
-					error = "OUT_OF_MEMORY";
-					break;
-				case GL46.GL_INVALID_FRAMEBUFFER_OPERATION:
-					error = "INVALID_FRAMEBUFFER_OPERATION";
-					break;
-			}
-			System.err.println(error + " (" + fileName + ".java:" + line);
-		}
-		return errorCode;
-
-	}
+	
 
 	private static enum Severity {
 		High, Medium, Low, Notification, Other
