4d467ce7683fb09d593570741bdc8cf509506947
Mistress_Meeple <jskynol001@gmail.com>
2020-08-02 21:09:08 +0100

    Adding comments/java-doc

--- src/com/meeple/backend/ShaderPrograms.java
+++ src/com/meeple/backend/ShaderPrograms.java
@@ -19,27 +19,96 @@ import com.meeple.shared.frame.OGL.ShaderProgram.GLDataType;
 import com.meeple.shared.frame.OGL.ShaderProgram.GLShaderType;
 import com.meeple.shared.frame.OGL.ShaderProgramSystem2;
 
+/**
+ * A helper class that holds all of the programs I use in the project and the attributes that are used in the programs. 
+ * Sits on top of the {@link ShaderProgramSystem2} class
+ * @author Megan
+ *
+ */
 public class ShaderPrograms {
 	private static Logger logger = Logger.getLogger(ShaderPrograms.class);
 
-	public final static AttributeFactory vertAtt = new AttributeFactory().name("vertex").bufferType(BufferType.ArrayBuffer).dataType(GLCompoundDataType.Vec3f).bufferUsage(BufferUsage.StaticDraw);
+	/**
+	 * Vertex position attribute used in all shaders.
+	 * <ul>
+	 * <li>Buffer Type: {@link BufferType.ArrayBuffer}</li>
+	 * <li>Data Type: {@link GLCompoundDataType.Vec3f}</li>
+	 * <li>Buffer Usage: {@link BufferUsage.StaticDraw}</li>
+	 * </ul>
+	 */
 
+	public final static AttributeFactory vertAtt = new AttributeFactory().name("vertex").bufferType(BufferType.ArrayBuffer).dataType(GLCompoundDataType.Vec3f).bufferUsage(BufferUsage.StaticDraw);
+	/**
+	 * Vertex normal attribute used in some shaders.
+	 * <ul>
+	 * <li>Buffer Type: {@link BufferType.ArrayBuffer}</li>
+	 * <li>Data Type: {@link GLCompoundDataType.Vec4f}</li>
+	 * <li>Buffer Usage: {@link BufferUsage.StaticDraw}</li>
+	 * </ul>
+	 */
 	public final static AttributeFactory normalAtt = new AttributeFactory().name("normal").bufferType(BufferType.ArrayBuffer).dataType(GLCompoundDataType.Vec4f).bufferUsage(BufferUsage.StaticDraw).dataSize(4);
 
+	/**
+	 * Model/vertex colour attribute used in some shaders.
+	 * <ul>
+	 * <li>Buffer Type: {@link BufferType.ArrayBuffer}</li>
+	 * <li>Data Type: {@link GLCompoundDataType.Vec4f}</li>
+	 * <li>Buffer Usage: {@link BufferUsage.StaticDraw}</li>
+	 * </ul>
+	 */
 	public final static AttributeFactory colourAtt = new AttributeFactory().name("colour").bufferType(BufferType.ArrayBuffer).dataType(GLCompoundDataType.Vec4f).bufferUsage(BufferUsage.DynamicDraw).dataSize(4);
 
+	/**
+	 * Transform/model matrix used in most shaders.
+	 * <ul>
+	 * <li>Buffer Type: {@link BufferType.ArrayBuffer}</li>
+	 * <li>Data Type: {@link GLCompoundDataType.Mat4f}</li>
+	 * <li>Buffer Usage: {@link BufferUsage.DynamicDraw}</li>
+	 * <li>Instanced: true, 1</li>
+	 * </ul>
+	 */
 	public final static AttributeFactory transformAtt = new AttributeFactory().name("meshMatrix").bufferType(BufferType.ArrayBuffer).dataType(GLCompoundDataType.Mat4f).bufferUsage(BufferUsage.DynamicDraw).instanced(true, 1);
-
+	/**
+	 * Model's normal matrix attribute used in some shaders. Used with the
+	 * {@link #normalAtt} and {@link #transformAtt}
+	 * <ul>
+	 * <li>Buffer Type: {@link BufferType.ArrayBuffer}</li>
+	 * <li>Data Type: {@link GLCompoundDataType.Mat4f}</li>
+	 * <li>Buffer Usage: {@link BufferUsage.DynamicDraw}</li>
+	 * <li>Instanced: true, 1</li>
+	 * </ul>
+	 */
 	public final static AttributeFactory normalMatrixAtt = new AttributeFactory().name("normalMatrix").bufferType(BufferType.ArrayBuffer).dataType(GLCompoundDataType.Mat4f).bufferUsage(BufferUsage.DynamicDraw).instanced(true, 1);
-
+	/**
+	 * Model's matieral index attribute used in some shaders. upload the materials
+	 * seperated and use this to specify which material the model will use
+	 * <ul>
+	 * <li>Buffer Type: {@link BufferType.ArrayBuffer}</li>
+	 * <li>Data Type: {@link GLCompoundDataType.Float}</li>
+	 * <li>Buffer Usage: {@link BufferUsage.DynamicDraw}</li>
+	 * <li>Instanced: true, 1</li>
+	 * </ul>
+	 */
 	public final static AttributeFactory materialIndexAtt = new AttributeFactory().name("materialIndex").bufferType(BufferType.ArrayBuffer).dataType(GLDataType.Float).bufferUsage(BufferUsage.DynamicDraw).instanced(true, 1);
 
 	public static enum InstancedAttribute {
 		Transformation, NormalMatrix, MaterialIndex, Colour;
 	}
 
+	/**
+	 * All of the shader programs used in this project stored in an enum. Before use
+	 * ensure the program has been constructed and initialized. <br>
+	 * This can be achieved in two ways. 
+	 * <ol>
+	 * <li>Calling both {@link ShaderPrograms#init(Program)} and {@link ShaderPrograms#create(GLContext, Program)}. These methods are both called in order in the {@link ShaderPrograms#initAndCreate(GLContext, Program)} method</li>
+	 * <li>Or programming it yourself as direct access to the shader program is available with the {@linkplain #program} variable</li>
+	 * </ol>
+	 * @author Megan
+	 */
 	public static enum Program {
-		_3D_Unlit_Flat, _3D_Lit_Flat, _3D_Lit_Material;
+		_3D_Unlit_Flat,
+		_3D_Lit_Flat,
+		_3D_Lit_Material;
 		public final ShaderProgram program;
 		private boolean init = false;
 		private boolean created = false;
@@ -49,6 +118,11 @@ public class ShaderPrograms {
 		}
 	}
 
+	/**
+	 * A helper class designed to manage the general instance data of models/meshes. 
+	 * @author Megan
+	 *
+	 */
 	public static class InstanceData {
 		private final Set<Instance> instances = Collections.synchronizedSet(new HashSet<>());
 
@@ -73,7 +147,11 @@ public class ShaderPrograms {
 		}
 	}
 
-	public static ShaderProgram init(Program program) {
+	/**
+	 * Initialises the program by supplying it the correct shader sources. 
+	 * @param program
+	 */
+	public static void init(Program program) {
 		String vert = "";
 		String frag = "";
 
@@ -96,20 +174,29 @@ public class ShaderPrograms {
 			program.program.shaderSources.put(GLShaderType.FragmentShader, frag);
 			program.init = true;
 		}
-		return program.program;
+
 	}
 
-	public static ShaderProgram initAndCreate(GLContext glc, Program programD) {
-		ShaderProgram program = init(programD);
-		if (!programD.created) {
+	/**
+	 * Creates the shader program
+	 * @param glc
+	 * @param program
+	 */
+	public static void create(GLContext glc, Program program) {
+		if (!program.created) {
 			try {
-				ShaderProgramSystem2.create(glc, program);
-				programD.created = true;
+				ShaderProgramSystem2.create(glc, program.program);
+				program.created = true;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		return program;
+	}
+
+	public static ShaderProgram initAndCreate(GLContext glc, Program programEnum) {
+		init(programEnum);
+		create(glc, programEnum);
+		return programEnum.program;
 	}
 
 }
